d6c64d42755411304242ce0b75b37d8287021c8f
RenaKgit <90835528+RenaKgit@users.noreply.github.com>
2023-07-12 15:23:21 -0500

    UserProfile (#2)
    Created user profile endpoint

---
+++ user_management/urls.py
@@ -0,0 +1,6 @@
+from django.urls import include, path
+from .views import UserProfileCreate
+
+
+urlpatterns = [
+    path('create-profile/', UserProfileCreate.as_view(), name='create-user'),]
--- user_management/views.py
+++ user_management/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import UserInfo
+from rest_framework import generics
+from .serializers import UserSerializer
 
-# Create your views here.
+
+class UserProfileCreate(generics.CreateAPIView):
+    # API endpoint that allows creation of a new customer
+    queryset = UserInfo.objects.all(),
+    serializer_class = UserSerializer
