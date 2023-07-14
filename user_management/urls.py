32d871aa2e902d1fbde269cd319cffdfc03063ea
RenaKgit <90835528+RenaKgit@users.noreply.github.com>
2023-07-14 17:57:01 -0500

    Code Coverage
    Installed coverage package

--- user_management/urls.py
+++ user_management/urls.py
@@ -3,4 +3,5 @@ from .views import UserProfileCreate
 
 
 urlpatterns = [
+
     path('create-profile/', UserProfileCreate.as_view(), name='create-user'),]
--- user_management/views.py
+++ user_management/views.py
@@ -6,5 +6,6 @@ from .serializers import UserSerializer
 
 class UserProfileCreate(generics.CreateAPIView):
     # API endpoint that allows creation of a new customer
+
     queryset = UserInfo.objects.all(),
     serializer_class = UserSerializer
