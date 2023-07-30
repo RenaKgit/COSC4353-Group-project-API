56425b33e05928b83b12bd24045caab3b1148e35
RenaKgit <90835528+RenaKgit@users.noreply.github.com>
2023-07-30 18:43:25 -0500

    Added Pricing Module (#5)
    Created a pricing module view to return the price.

--- user_management/urls.py
+++ user_management/urls.py
@@ -1,7 +1,8 @@
 from django.urls import include, path
-from .views import UserProfileCreate
+from .views import UserProfileCreate, GetFuelQuote
 
 
 urlpatterns = [
 
-    path('create-profile/', UserProfileCreate.as_view(), name='create-user'),]
+    path('create-profile/', UserProfileCreate.as_view(), name='create-user'),
+    path('quote-module/', GetFuelQuote.as_view(), name='quote-module'),]
